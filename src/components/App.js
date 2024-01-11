923f70000ba8be52c90e7e2136fc473a1c741a72
benjamindlakic <benjomaster7@gmail.com>
2024-01-11 17:12:31 +0100

    add, buy

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import Web3 from 'web3'
 import logo from '../logo.png';
 import './App.css';
-import Web3 from 'web3';
-import Navbar from './Navbar';
-
+import Marketplace from '../abis/Marketplace.json'
+import Navbar from './Navbar'
+import Main from './Main'
 
 class App extends Component {
+
   async componentWillMount() {
     await this.loadWeb3()
     await this.loadBlockchainData()
   }
+
   async loadWeb3() {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum)
@@ -25,12 +28,27 @@ class App extends Component {
 
   async loadBlockchainData() {
     const web3 = window.web3
-  }
-
-  async loadBlockchainData() {
-    const web3 = window.web3
+    // Load account
     const accounts = await web3.eth.getAccounts()
     this.setState({ account: accounts[0] })
+    const networkId = await web3.eth.net.getId()
+    const networkData = Marketplace.networks[networkId]
+    if(networkData) {
+      const marketplace = new web3.eth.Contract(Marketplace.abi, networkData.address)
+      this.setState({ marketplace })
+      const productCount = await marketplace.methods.productCount().call()
+      this.setState({ productCount })
+      // Load products
+      for (var i = 1; i <= productCount; i++) {
+        const product = await marketplace.methods.products(i).call()
+        this.setState({
+          products: [...this.state.products, product]
+        })
+      }
+      this.setState({ loading: false})
+    } else {
+      window.alert('Marketplace contract not deployed to detected network.')
+    }
   }
 
   constructor(props) {
@@ -41,6 +59,25 @@ class App extends Component {
       products: [],
       loading: true
     }
+
+    this.createProduct = this.createProduct.bind(this)
+    this.purchaseProduct = this.purchaseProduct.bind(this)
+  }
+
+  createProduct(name, price) {
+    this.setState({ loading: true })
+    this.state.marketplace.methods.createProduct(name, price).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  purchaseProduct(id, price) {
+    this.setState({ loading: true })
+    this.state.marketplace.methods.purchaseProduct(id).send({ from: this.state.account, value: price })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
   }
 
   render() {
@@ -49,28 +86,14 @@ class App extends Component {
         <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
+            <main role="main" className="col-lg-12 d-flex">
+              { this.state.loading
+                ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+                : <Main
+                  products={this.state.products}
+                  createProduct={this.createProduct}
+                  purchaseProduct={this.purchaseProduct} />
+              }
             </main>
           </div>
         </div>
